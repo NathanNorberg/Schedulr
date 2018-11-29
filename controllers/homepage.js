5a6f98d0eb0d3657e2dc108c1893dbaf6243f8c8
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-11-28 21:01:01 -0700

    dynamically rendering unassigned drivers and routes in sidenav

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -4,18 +4,22 @@ const scheduler = require('../config/utils/scheduler.js');
 module.exports = {
 
   index: function(req, res) {
-
+    // pull data from relevant tables returning as a Promise object
     let routePromise = knex('routes').where('company_id', req.session.user.id)
     let driverPromise = knex('drivers').where('bossman', req.session.user.id)
     let truckPromise = knex('trucks').where('owner', req.session.user.id)
 
+    // wait for promises to resolve, avoids race condition
     Promise.all([routePromise, driverPromise, truckPromise])
       .then( (results) => {
+        // extract data from the promises array that was processed by Promise.all
         let routes = results[0];
         let drivers = results[1];
         let trucks = results[2];
+        console.log(routes);
 
-        res.render('homepage', {routes: routes, drivers: drivers, trucks: trucks});
+        // pass data to homepage repackaged with 'readable' names
+        res.render('homepage', {user: req.session.user, routes: routes, drivers: drivers, trucks: trucks});
       })
   },
 
--- db/seeds/04_routes.js
+++ db/seeds/04_routes.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('routes').insert([
         {name: 'TJ108', dock_length: 3, day: 'mon', recurring: false, pickup_time: '01:30:00', destination: 'Valencia, CA', company_id: 3},
-        {name: 'TJ108', dock_length: 3, day: 'tues', recurring: false, pickup_time: '01:30:00', destination: 'Valencia, CA', company_id: 3},
+        {name: 'TJ108', dock_length: 3, day: 'tue', recurring: false, pickup_time: '01:30:00', destination: 'Valencia, CA', company_id: 3},
         {name: 'TJ108', dock_length: 3, day: 'wed', recurring: false, pickup_time: '01:30:00', destination: 'Valencia, CA', company_id: 3},
         {name: 'TJ108', dock_length: 3, day: 'thur', recurring: false, pickup_time: '01:30:00', destination: 'Valencia, CA', company_id: 3},
         {name: 'TJ108', dock_length: 3, day: 'fri', recurring: false, pickup_time: '01:30:00', destination: 'Valencia, CA', company_id: 3},
