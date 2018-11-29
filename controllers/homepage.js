baa5c4f7540a76a9311c8f43c46de9dd5ea312d6
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-11-28 21:57:14 -0700

    working on dynamically generating table

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -13,13 +13,25 @@ module.exports = {
     Promise.all([routePromise, driverPromise, truckPromise])
       .then( (results) => {
         // extract data from the promises array that was processed by Promise.all
-        let routes = results[0];
+        let unassignedRoutes = results[0].filter(route => route.driver_id == null || route.truck_id == null);
+        let assignedRoutes = results[0].filter(route => route.driver_id != null && route.truck_id != null);
         let drivers = results[1];
         let trucks = results[2];
-        console.log(routes);
+        // figure out how many routes appear on your busiest day
+        let longestDay = Math.max(
+          assignedRoutes.filter(route => route.day === 'mon').length,
+          assignedRoutes.filter(route => route.day === 'tue').length,
+          assignedRoutes.filter(route => route.day === 'wed').length,
+          assignedRoutes.filter(route => route.day === 'thur').length,
+          assignedRoutes.filter(route => route.day === 'fri').length,
+          assignedRoutes.filter(route => route.day === 'sat').length,
+          assignedRoutes.filter(route => route.day === 'sun').length
+        )
+        console.log(longestDay);
+
 
         // pass data to homepage repackaged with 'readable' names
-        res.render('homepage', {user: req.session.user, routes: routes, drivers: drivers, trucks: trucks});
+        res.render('homepage', {user: req.session.user, assigned: assignedRoutes, unassigned: unassignedRoutes, drivers: drivers, trucks: trucks, routeCount: longestDay});
       })
   },
 
